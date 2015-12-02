c446999a0f9be5809a78cd729b971b141372e376
Aleksey <a.povarkov@gmail.com>
2015-12-02 12:39:37 +0300

    Date Picker Test

---
+++ src/main/java/com/example/chapter2/Recipe02_10.java
@@ -0,0 +1,41 @@
+package com.example.chapter2;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+
+/**
+ * Created by apovarkov on 02.12.15.
+ */
+public class Recipe02_10 extends Application {
+    private Label dateLabel;
+    private DatePicker datePicker;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        dateLabel = new Label("Select a date using the widget");
+        datePicker = new DatePicker(LocalDate.now());
+        datePicker.setOnAction(event -> {
+            dateLabel.setText("The selected date is: " + datePicker.getValue());
+        });
+        FlowPane flow = new FlowPane();
+        flow.setPadding(new Insets(5, 5, 5, 5));
+        flow.getChildren().add(dateLabel);
+        flow.getChildren().add(datePicker);
+        primaryStage.setScene(new Scene(flow, 300, 100));
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
