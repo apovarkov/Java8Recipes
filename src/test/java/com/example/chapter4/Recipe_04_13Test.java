fc829dd01f7b7ffd1b1f0da9b2fd90d92ebceb51
Aleksey <a.povarkov@gmail.com>
2016-01-14 13:48:27 +0300

    ZonedDateTime sample

--- src/test/java/com/example/chapter4/Recipe_04_13Test.java
+++ src/test/java/com/example/chapter4/Recipe_04_13Test.java
@@ -3,6 +3,8 @@ package com.example.chapter4;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.ZonedDateTime;
 
 /**
@@ -15,6 +17,11 @@ public class Recipe_04_13Test extends TestCase {
         System.out.println("Zone is: " + zonedDateTime.getZone());
         System.out.println("Zone rules are: " + zonedDateTime.getZone().getRules());
         assertFalse(false);
+
+        LocalDate anotherDate = LocalDate.of(2015, 12, 12);
+        ZonedDateTime anotherDateTime = ZonedDateTime.of(anotherDate, LocalTime.of(12, 12), zonedDateTime.getZone());
+
+        assertTrue(zonedDateTime.compareTo(anotherDateTime) > 0);
     }
 
 }
