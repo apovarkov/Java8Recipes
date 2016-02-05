45c92c98084db276cfce7cd798a9d40dd05584bb
Aleksey <a.povarkov@gmail.com>
2016-02-05 11:12:39 +0300

    Return lambda

---
+++ src/main/java/com/example/chapter6/Recipe_06_02.java
@@ -0,0 +1,10 @@
+package com.example.chapter6;
+
+/**
+ * Created by apovarkov on 05.02.16.
+ */
+public class Recipe_06_02 {
+    public interface ReverseType {
+        String reverse(String text);
+    }
+}
---
+++ src/test/java/com/example/chapter6/Recipe_06_02Test.java
@@ -0,0 +1,42 @@
+package com.example.chapter6;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by apovarkov on 05.02.16.
+ */
+public class Recipe_06_02Test extends TestCase {
+    public static final String ONE_WORD_EXPECTATION = "OLLEH";
+    public static final String MULTI_WORD_EXPECTATION = "OLLEH DLROW";
+    public static final String SPACE = " ";
+    public static final String EMPTY_STRING = "";
+    private Recipe_06_02.ReverseType lambda;
+
+    @Before
+    public void setUp() {
+        lambda = (testText) -> {
+            String tempStr = EMPTY_STRING;
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String part : testText.split(SPACE)) {
+                stringBuilder.append(new StringBuilder(part).reverse()).append(SPACE);
+            }
+            tempStr = stringBuilder.toString().trim();
+            return tempStr;
+        };
+    }
+
+    @Test
+    public void testLambda() {
+        assertEquals(ONE_WORD_EXPECTATION, lambda.reverse("HELLO"));
+        assertEquals(MULTI_WORD_EXPECTATION, lambda.reverse("HELLO WORLD"));
+    }
+
+    @After
+    public void tearDown() {
+        lambda = null;
+    }
+
+}
