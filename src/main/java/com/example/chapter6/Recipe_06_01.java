8de4491b622cd7b254d1ae87160d0229bf906b95
Aleksey <a.povarkov@gmail.com>
2016-02-19 11:48:42 +0300

    FunctionalInterface annotation has been added

--- src/main/java/com/example/chapter6/Recipe_06_01.java
+++ src/main/java/com/example/chapter6/Recipe_06_01.java
@@ -4,9 +4,11 @@ package com.example.chapter6;
  * Created by apovarkov on 05.02.16.
  */
 public class Recipe_06_01 {
+    @FunctionalInterface
     public interface HelloType {
         /**
          * Function that will be implemented within the lambda
+         *
          * @param text
          */
         void hello(String text);
--- src/main/java/com/example/chapter6/Recipe_06_02.java
+++ src/main/java/com/example/chapter6/Recipe_06_02.java
@@ -4,6 +4,7 @@ package com.example.chapter6;
  * Created by apovarkov on 05.02.16.
  */
 public class Recipe_06_02 {
+    @FunctionalInterface
     public interface ReverseType {
         String reverse(String text);
     }
